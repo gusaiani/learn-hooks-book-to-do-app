3596e0afc5dca65a4eff8969ebebfbbd53793f1d
Gustavo Saiani <gustavo@poe.ma>
2020-02-06 20:38:51 -0300

    Make static version

---
+++ src/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class AddTodo extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Enter new task…" style={{width: 350, height: 15}} />
+        <input type="submit" style={{float: 'right', marginTop: 2}} value="add" />
+      </form>
+    )
+  }
+}
